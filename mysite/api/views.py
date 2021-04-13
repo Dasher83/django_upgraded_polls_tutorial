0d9dbcbdc9178b58b05444913850a661371b02e9
Guga <agustin.fernandez@xmartlabs.com>
2021-04-13 10:59:37 -0300

    Added the missing override of the get_serializer_class method

--- mysite/api/views.py
+++ mysite/api/views.py
@@ -2,9 +2,14 @@
 from rest_framework import viewsets
 
 from polls.models import Question, Choice, User
-from .serializers import QuestionSerializer, ChoiceSerializer, \
-    UserRetrieveSerializer, UserListSerializer, UserCreateSerializer, \
-    UserUpdateSerializer
+from .serializers import (
+    QuestionSerializer,
+    ChoiceSerializer,
+    UserRetrieveSerializer,
+    UserListSerializer,
+    UserCreateSerializer,
+    UserUpdateSerializer,
+)
 
 
 class QuestionViewSet(viewsets.ModelViewSet):
@@ -21,7 +26,13 @@ class UserViewSet(viewsets.ModelViewSet):
     serializer_class = UserRetrieveSerializer
     queryset = User.objects.all()
     serializer_action_classes = {
-        'list': UserListSerializer,
-        'create': UserCreateSerializer,
-        'update': UserUpdateSerializer
+        "list": UserListSerializer,
+        "create": UserCreateSerializer,
+        "update": UserUpdateSerializer,
     }
+
+    def get_serializer_class(self):
+        try:
+            return self.serializer_action_classes[self.action]
+        except (KeyError, AttributeError):
+            return super().get_serializer_class()
